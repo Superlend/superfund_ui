47f8279329a03b79f8dfb7c5a85140cd81099c5f
shreyaspangal <shreyaspangal03@gmail.com>
2025-01-24 15:41:37 +0530

    add: page header with logo

--- app/super-fund/page-header.tsx
+++ app/super-fund/page-header.tsx
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import { motion } from "motion/react"
-import { HeadingText } from '@/components/ui/typography'
+import { HeadingText, Label } from '@/components/ui/typography'
 import useIsClient from '@/hooks/useIsClient';
 import { Skeleton } from '@/components/ui/skeleton';
 import ImageWithDefault from '@/components/ImageWithDefault';
+import { Badge } from '@/components/ui/badge';
 
 export default function PageHeader() {
 
@@ -16,14 +17,34 @@ export default function PageHeader() {
             transition={{ duration: 0.5, ease: "easeOut" }}
         >
             <div className="flex items-center gap-2">
-                {/* <ImageWithDefault src={'/images/logos/superlend-rounded.svg'}
+                <ImageWithDefault src={'/images/logos/superlend-rounded.svg'}
                     alt="Bluechip Stable SuperFund"
-                    width={32}
-                    height={32}
-                /> */}
-                <HeadingText level="h2" weight="medium">
+                    width={28}
+                    height={28}
+                />
+                <HeadingText level="h4" weight="medium" className="mr-1">
                     Bluechip Stable SuperFund
                 </HeadingText>
+                <Badge
+                    size="md"
+                    className="border-0 flex items-center justify-between gap-[16px] px-[6px] w-fit max-w-[400px]"
+                >
+                    <div className="flex items-center gap-1">
+                        <ImageWithDefault
+                            src={'https://superlend-assets.s3.ap-south-1.amazonaws.com/base.svg'}
+                            alt={`Base`}
+                            width={16}
+                            height={16}
+                            className="object-contain shrink-0 max-w-[16px] max-h-[16px]"
+                        />
+                        <Label
+                            weight="medium"
+                            className="leading-[0] shrink-0 capitalize"
+                        >
+                            Base
+                        </Label>
+                    </div>
+                </Badge>
             </div>
         </motion.div>
     )
