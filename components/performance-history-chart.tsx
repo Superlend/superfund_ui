181b7d42470af159e34a1c6f9e27dd1a5de68aef
shreyaspangal <shreyaspangal03@gmail.com>
2025-02-11 18:12:17 +0530

    feat: Enhance performance history chart with improved axis formatting and brush functionality

--- components/performance-history-chart.tsx
+++ components/performance-history-chart.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Card } from './ui/card'
-import { motion } from 'framer-motion'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import {
     Area,
@@ -16,22 +15,10 @@ import {
 } from 'recharts'
 import { Period } from '@/types/periodButtons'
 import { PERIOD_LIST } from '@/constants'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useHistoricalData } from '@/hooks/vault_hooks/useHistoricalDataHook'
-import { abbreviateNumber, extractTimeFromDate } from '@/lib/utils'
-
-const data = [
-    { date: '12:05 27 Jan 2025', baseApy: 14, totalApy: 17, totalAssets: 14 },
-    { date: '12:05 28 Jan 2025', baseApy: 16, totalApy: 19, totalAssets: 16 },
-    { date: '12:05 29 Jan 2025', baseApy: 13, totalApy: 16, totalAssets: 13 },
-    { date: '12:05 30 Jan 2025', baseApy: 15, totalApy: 18, totalAssets: 15 },
-    { date: '12:05 31 Jan 2025', baseApy: 16, totalApy: 19, totalAssets: 16 },
-    { date: '12:05 01 Feb 2025', baseApy: 14, totalApy: 17, totalAssets: 14 },
-    { date: '12:05 02 Feb 2025', baseApy: 18, totalApy: 21, totalAssets: 18 },
-    { date: '12:05 03 Feb 2025', baseApy: 19, totalApy: 22, totalAssets: 19 },
-    { date: '12:05 04 Feb 2025', baseApy: 21, totalApy: 23, totalAssets: 21 },
-    { date: '12:05 05 Feb 2025', baseApy: 22, totalApy: 24, totalAssets: 22 },
-]
+import { abbreviateNumber, extractTimeFromDate, formatDateAccordingToPeriod, shortNubers } from '@/lib/utils'
+import { ChartConfig } from './ui/chart'
 
 const CustomTooltip = ({ active, payload }: any) => {
     if (active && payload && payload.length) {
@@ -76,9 +63,89 @@ const styles = `
     }
 `
 
+interface CustomXAxisTickProps {
+    x: number
+    y: number
+    selectedRange: Period
+    payload: {
+        value: number
+    }
+    index: number
+    length: number
+}
+
+interface CustomYAxisTickProps {
+    x: number
+    y: number
+    payload: {
+        value: number
+    }
+    index: number
+    length: number
+    setYAxisDigitCount: any
+}
+
+const CustomYAxisTick = ({
+    x,
+    y,
+    payload,
+    index,
+    length,
+    setYAxisDigitCount,
+}: CustomYAxisTickProps) => {
+    // if (index === 0 || index === length - 1) return null
+    // setYAxisDigitCount(payload.value.toString().length)
+
+    return (
+        <g
+            transform={`translate(${x},${y})`}
+            style={{ zIndex: 10, position: 'relative', color: 'hsl(var(--foreground-subtle))' }}
+        >
+            <text x={0} y={0} dy={6} dx={11} textAnchor="start" fontSize={12} fill="hsl(var(--foreground-subtle))">
+                {`${shortNubers(payload.value)}%`}
+            </text>
+        </g>
+    )
+}
+
+const CustomXAxisTick = ({
+    x,
+    y,
+    selectedRange,
+    payload,
+    index,
+    length,
+}: CustomXAxisTickProps) => {
+    if (index % 2) return null
+    return (
+        <g transform={`translate(${x + 10},${y})`} style={{ zIndex: 10 }}>
+            <text x={0} y={0} dy={16} textAnchor="middle" fontSize={12} fill="hsl(var(--foreground-subtle))">
+                {formatDateAccordingToPeriod(
+                    payload.value.toString(),
+                    selectedRange
+                )}
+            </text>
+        </g>
+    )
+}
+
+const chartConfig = {
+    platformHistory: {
+        label: 'History',
+        color: 'hsl(var(--chart-2))',
+    },
+} satisfies ChartConfig
+
 export default function PerformanceHistoryChart() {
     const [selectedRange, setSelectedRange] = useState<Period>(Period.oneMonth)
     const { historicalData } = useHistoricalData(selectedRange)
+    const [startIndex, setStartIndex] = useState(0)
+    const [endIndex, setEndIndex] = useState(historicalData.length - 1)
+
+    useEffect(() => {
+        setStartIndex(0)
+        setEndIndex(historicalData.length - 1)
+    }, [historicalData])
 
     const chartData = historicalData.map((item: any) => {
         const date = new Date(item.timestamp * 1000)
@@ -106,6 +173,11 @@ export default function PerformanceHistoryChart() {
     const handleRangeChange = (value: string) => {
         setSelectedRange(value as Period)
     }
+
+    const allValues = chartData.flatMap(d => [Number(d.baseApy), Number(d.totalApy)]);
+    const minValue = Math.min(...allValues);
+    const maxValue = Math.max(...allValues);
+    const valueRange = maxValue - minValue;
 
     return (
         <Card>
@@ -145,44 +217,6 @@ export default function PerformanceHistoryChart() {
                             bottom: 45
                         }}
                     >
-                        {/* <defs>
-                            <linearGradient
-                                id="baseApyGradient"
-                                x1="0"
-                                y1="0"
-                                x2="0"
-                                y2="1"
-                            >
-                                <stop
-                                    offset="5%"
-                                    stopColor="hsl(var(--chart-superlend))"
-                                    stopOpacity={0.15}
-                                />
-                                <stop
-                                    offset="95%"
-                                    stopColor="hsl(var(--chart-superlend))"
-                                    stopOpacity={0.01}
-                                />
-                            </linearGradient>
-                            <linearGradient
-                                id="totalApyGradient"
-                                x1="0"
-                                y1="0"
-                                x2="0"
-                                y2="1"
-                            >
-                                <stop
-                                    offset="5%"
-                                    stopColor="hsl(var(--chart-blue))"
-                                    stopOpacity={0.15}
-                                />
-                                <stop
-                                    offset="95%"
-                                    stopColor="hsl(var(--chart-blue))"
-                                    stopOpacity={0.01}
-                                />
-                            </linearGradient>
-                        </defs> */}
                         <XAxis
                             dataKey="date"
                             stroke="hsl(var(--foreground-subtle))"
@@ -192,49 +226,186 @@ export default function PerformanceHistoryChart() {
                             axisLine={false}
                             padding={{ left: 10, right: 10 }}
                             dy={10}
+                            tick={({ x, y, payload, index }) => (
+                                <CustomXAxisTick
+                                    payload={
+                                        payload as { value: number }
+                                    }
+                                    selectedRange={selectedRange}
+                                    x={x as number}
+                                    y={y as number}
+                                    index={index as number}
+                                    length={chartData.length}
+                                />
+                            )}
                         />
                         <YAxis
                             stroke="hsl(var(--foreground-subtle))"
                             fontSize={12}
                             tickLine={false}
                             axisLine={false}
-                            tickFormatter={(value) => `${value}%`}
+                            tickFormatter={(value) => `${shortNubers(value.toFixed(2))}%`}
                             padding={{ top: 10, bottom: 10 }}
-                            dx={-10}
+                            domain={[
+                                minValue - (valueRange * 0.1), 
+                                maxValue + (valueRange * 0.1)
+                            ]}
+                            allowDataOverflow={true}
                         />
                         <Tooltip
                             content={<CustomTooltip />}
                             cursor={{ stroke: 'hsl(var(--foreground-disabled))', strokeWidth: 1 }}
                         />
-                        {/* Base APY Line */}
                         <Line
                             type="monotone"
                             dataKey="baseApy"
                             stroke="hsl(var(--chart-morpho))"
                             strokeWidth={2}
-                            // dot={{ fill: "hsl(var(--chart-morpho))", r: 4 }}
-                            // activeDot={{ r: 3, strokeWidth: 0 }}
-                            // fill="url(#baseApyGradient)"
+                            fill="url(#baseApyGradient)"
                             isAnimationActive={true}
+                            dot={false}
+
                         />
-                        {/* Total APY Line */}
                         <Line
                             type="monotone"
                             dataKey="totalApy"
                             stroke="hsl(var(--chart-superlend))"
                             strokeWidth={2}
-                            // dot={{ fill: "hsl(var(--chart-superlend))", r: 4 }}
-                            // activeDot={{ r: 3, strokeWidth: 0 }}
-                            // fill="url(#totalApyGradient)"
+                            fill="url(#totalApyGradient)"
                             isAnimationActive={true}
+                            dot={false}
                         />
                         <Brush
                             dataKey="date"
                             height={35}
                             stroke="hsl(var(--pulse-color))"
-                            // fill="url(#baseApyGradient)"
+                            fill="hsl(var(--accent-cream))"
                             travellerWidth={8}
                             y={245}
+                            strokeWidth={1.2}
+                            startIndex={startIndex}
+                            endIndex={endIndex}
+                            className="recharts-brush"
+                            alwaysShowText={false}
+                        >
+                            <AreaChart>
+                                <Area
+                                    type="monotone"
+                                    dataKey="baseApy"
+                                    stroke="hsl(var(--chart-morpho))"
+                                    // fill="hsla(var(--chart-morpho), 0.15)"
+                                    strokeWidth={1}
+                                />
+                                <Area
+                                    type="monotone"
+                                    dataKey="totalApy"
+                                    stroke="hsl(var(--chart-superlend))"
+                                    // fill="hsla(var(--chart-superlend), 0.15)"
+                                    strokeWidth={1}
+                                />
+                            </AreaChart>
+                        </Brush>
+                    </LineChart>
+                </ResponsiveContainer >
+            </div >
+        </Card >
+    )
+}
+
+{/* <ChartContainer
+                        config={chartConfig}
+                        className="h-[250px] w-full"
+                    >
+                        <AreaChart
+                            accessibilityLayer
+                            data={chartData}
+                            margin={{
+                                left: 10,
+                                right: 20,
+                                top: 30,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid vertical={false} />
+                            <XAxis
+                                dataKey="date"
+                                tickLine={true}
+                                axisLine={true}
+                                tickMargin={5}
+                                // interval={100}
+                                tickCount={4}
+                                tickFormatter={(value) =>
+                                    formatDateAccordingToPeriod(
+                                        value,
+                                        selectedRange
+                                    )
+                                }
+                                tick={({ x, y, payload, index }) => (
+                                    <CustomXAxisTick
+                                        payload={
+                                            payload as { value: number }
+                                        }
+                                        selectedRange={selectedRange}
+                                        x={x as number}
+                                        y={y as number}
+                                        index={index as number}
+                                        length={chartData.length}
+                                    />
+                                )}
+                            />
+                            <ChartTooltip
+                                cursor={true}
+                                content={
+                                    <ChartTooltipContent />
+                                }
+                            />
+                            <Area
+                                dataKey="totalApy"
+                                type="monotone"
+                                fill="hsl(var(--chart-superlend))"
+                                fillOpacity={0.3}
+                                stroke="hsl(var(--chart-superlend))"
+                                strokeWidth={2}
+                                stackId="a"
+                                activeDot={{ r: 6 }}
+                            />
+                            <Area
+                                dataKey="baseApy"
+                                type="monotone"
+                                fill="var(--color-platformHistory)"
+                                fillOpacity={0.3}
+                                stroke="var(--color-platformHistory)"
+                                strokeWidth={2}
+                                stackId="a"
+                                activeDot={{ r: 6 }}
+                            />
+                            <YAxis
+                                tick={({ x, y, payload, index }) => (
+                                    <CustomYAxisTick
+                                        payload={
+                                            payload as { value: number }
+                                        }
+                                        x={x as number}
+                                        y={y as number}
+                                        index={index as number}
+                                        length={chartData.length}
+                                        setYAxisDigitCount={4}
+                                    />
+                                )}
+                                // domain={[minValue, maxValue]}
+                                tickCount={4}
+                                tickMargin={40}
+                                // stroke="#FFF"
+                                tickLine={true}
+                                axisLine={true}
+                            />
+                            <Brush
+                            dataKey="date"
+                            height={35}
+                            stroke="hsl(var(--pulse-color))"
+                            fill="url(#baseApyGradient)"
+                            travellerWidth={8}
+                            y={255}
                             strokeWidth={1.2}
                             startIndex={0}
                             endIndex={data.length - 1}
@@ -258,9 +429,5 @@ export default function PerformanceHistoryChart() {
                                 />
                             </AreaChart>
                         </Brush>
-                    </LineChart>
-                </ResponsiveContainer >
-            </div >
-        </Card >
-    )
-}
+                        </AreaChart>
+                    </ChartContainer> */}
