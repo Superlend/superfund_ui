298cdf8cb37e6723bfff264d2befe5fea1838a34
shreyaspangal <shreyaspangal03@gmail.com>
2025-04-10 16:15:27 +0530

    fix: Update API path for effective APY retrieval to include specific vault ID

--- queries/effective-apy-api.ts
+++ queries/effective-apy-api.ts
@@ -3,6 +3,6 @@ import { requestIndexer } from './request'
 export async function getEffectiveApy() {
     return requestIndexer<any>({
         method: 'GET',
-        path: `vaults/state/0x10076ed296571cE4Fde5b1FDF0eB9014a880e47B`,
+        path: `vaults/state/0x10076ed296571cE4Fde5b1FDF0eB9014a880e47B/8453`,
     })
 }
