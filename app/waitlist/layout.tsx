db37232c3ca99beed75a2eb63febcb1674ad67ea
shreyaspangal <shreyaspangal03@gmail.com>
2025-04-22 14:36:09 +0530

    update: waitlist page metadata

--- app/waitlist/layout.tsx
+++ app/waitlist/layout.tsx
@@ -1,14 +1,69 @@
 import type { Metadata } from 'next'
 import '../globals.css'
 import Link from 'next/link'
-import { Badge } from '@/components/ui/badge'
-import ContextProvider, { config, PRIVY_APP_ID } from '@/context'
+import ContextProvider from '@/context'
 import { headers } from 'next/headers'
 
+const frame = {
+    version: 'next',
+    imageUrl:
+        'https://superlend-assets.s3.ap-south-1.amazonaws.com/superlend_banner.png',
+    button: {
+        title: 'Check This Out',
+        action: {
+            type: 'launch_frame',
+            name: 'SuperFund',
+            url: 'https://funds.superlend.xyz/',
+            splashImageUrl:
+                'https://funds.superlend.xyz/images/logos/favicon-32x32.png',
+            splashBackgroundColor: '#B4E2FB',
+        },
+    },
+}
+
 export const metadata: Metadata = {
+    metadataBase: new URL('https://funds.superlend.xyz'),
+    alternates: {
+        canonical: '/',
+        languages: {
+            'en-US': '/en-US',
+        },
+    },
+    keywords: [
+        'DeFi',
+        'Deposit',
+        'Withdraw',
+        'DeFi rates',
+        'Earn in DeFi',
+        'Earn USDC',
+    ],
     title: 'Join SuperFund Waitlist - Earn Maximum Yield on USDC',
     description:
-        'Join the waitlist for SuperFund — an automated DeFi fund that allocates USDC across Aave, Morpho, Euler & more for optimized returns.',
+        'Earn smarter with SuperFund — an automated DeFi fund that allocates USDC across Aave, Morpho, Euler & more for optimized returns.',
+    icons: [
+        { url: '/images/logos/favicon-16x16.png', sizes: '16x16' },
+        { url: '/images/logos/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'apple-touch-icon', url: '/images/logos/apple-touch-icon.png' },
+        { rel: 'apple-touch-icon', url: '/images/logos/apple-touch-icon.png' },
+    ],
+    openGraph: {
+        type: 'website',
+        url: 'https://funds.superlend.xyz',
+        title: 'Join SuperFund Waitlist - Earn Maximum Yield on USDC',
+        description:
+            'Earn smarter with SuperFund — an automated DeFi fund that allocates USDC across Aave, Morpho, Euler & more for optimized returns.',
+        siteName: 'Superfund',
+        images: 'https://superlend-assets.s3.ap-south-1.amazonaws.com/superlend_banner.png',
+    },
+    twitter: {
+        title: 'Join SuperFund Waitlist - Earn Maximum Yield on USDC',
+        description:
+            'Earn smarter with SuperFund — an automated DeFi fund that allocates USDC across Aave, Morpho, Euler & more for optimized returns.',
+        images: 'https://superlend-assets.s3.ap-south-1.amazonaws.com/superlend_banner.png',
+    },
+    other: {
+        'fc:frame': JSON.stringify(frame),
+    },
 }
 
 export default function Layout({
