dd70b0fd05515dcac64b464318549e355e9e4770
tusharverma06 <vermatushar237@gmail.com>
2025-05-12 18:35:49 +0530

    calling ready for frames in layout

--- context/index.tsx
+++ context/index.tsx
@@ -4,12 +4,11 @@ import React, { useEffect, useState, type ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PrivyProvider } from '@privy-io/react-auth'
 import { createConfig, WagmiProvider } from '@privy-io/wagmi'
-import {
-    base,
-    sonic,
-} from 'viem/chains'
+import { base, sonic } from 'viem/chains'
 import { http } from 'wagmi'
 import { AuthProvider } from './auth-provider'
+import FrameSDK from '@farcaster/frame-sdk'
+import { farcasterFrame } from '@farcaster/frame-wagmi-connector'
 
 // Set up queryClient
 const queryClient = new QueryClient()
@@ -24,13 +23,13 @@ const metadata = {
     icons: ['https://avatars.githubusercontent.com/u/179229932'],
 }
 
-export const config = createConfig({
-    chains: [base, sonic],
-    transports: {
-        [base.id]: http(),
-        [sonic.id]: http(),
-    },
-})
+// export const config = createConfig({
+//     chains: [base, sonic],
+//     transports: {
+//         [base.id]: http(),
+//         [sonic.id]: http(),
+//     },
+// })
 
 function ContextProvider({
     children,
@@ -42,30 +41,33 @@ function ContextProvider({
     const [localConfig, setLocalConfig] = useState<any>(null)
     const [context, setContext] = useState<any>(null)
 
-    // useEffect(() => {
-    //     const initializeConfig = async () => {
-    //         await FrameSDK.actions.ready()
-    //         const context = await FrameSDK.context
-    //         setContext(context)
-    //         const newConfig = createConfig({
-    //             chains: [base],
-    //             transports: {
-    //                 [base.id]: http(),
-    //             },
-    //         })
-    //         const newConfigForFarcaster = createConfig({
-    //             chains: [base],
-    //             transports: {
-    //                 [base.id]: http(),
-    //             },
-    //             connectors: [farcasterFrame()],
-    //         })
+    useEffect(() => {
+        const initializeConfig = async () => {
+            await FrameSDK.actions.ready()
+            const context = await FrameSDK.context
+            setContext(context)
+            const newConfig = createConfig({
+                chains: [base, sonic],
+                transports: {
+                    [base.id]: http(),
+                    [sonic.id]: http(),
+                },
+            })
 
-    //         setLocalConfig(context ? newConfigForFarcaster : newConfig)
-    //     }
+            const newConfigForFarcaster = createConfig({
+                chains: [base, sonic],
+                transports: {
+                    [base.id]: http(),
+                    [sonic.id]: http(),
+                },
+                connectors: [farcasterFrame()],
+            })
 
-    //     initializeConfig()
-    // }, [])
+            setLocalConfig(context ? newConfigForFarcaster : newConfig)
+        }
+
+        initializeConfig()
+    }, [])
 
     return (
         <PrivyProvider
@@ -93,10 +95,8 @@ function ContextProvider({
             }}
         >
             <QueryClientProvider client={queryClient}>
-                <WagmiProvider config={config}>
-                    <AuthProvider>
-                        {children}
-                    </AuthProvider>
+                <WagmiProvider config={localConfig}>
+                    <AuthProvider>{children}</AuthProvider>
                 </WagmiProvider>
             </QueryClientProvider>
         </PrivyProvider>
